7ff42dfc1f3d96e8f206d1f11ada657833aa032d
Speedtron222 <79450117+Speedtron222@users.noreply.github.com>
2023-05-13 22:17:37 +0200

    Add files via upload

---
+++ M05_UF3_P3_Frogger-AntonioManuelÁlvarezMadueño/M05_UF3_P3_Frogger/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M05_UF3_P3_Frogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            List<Lane> lanes = new List<Lane>();
+            Utils.GAME_STATE gameState = Utils.GAME_STATE.RUNNING;
+            TimeManager.timer.Start();
+            bool speedPlayer;
+            ConsoleColor background;
+            bool damageElements;
+            bool damageBackground;
+            float elementsPercent;
+            char elementsChar;
+            List<ConsoleColor> colorsElements = Utils.colorsLogs.ToList();
+            for (int i = 1; i < Utils.MAP_HEIGHT-1; i++)
+            {
+                if (i<Utils.MAP_HEIGHT/2)
+                {
+                    speedPlayer = true;
+                    background = ConsoleColor.Cyan;
+                    damageElements = false;
+                    damageBackground = true;
+                    elementsPercent = 0.5f; //Spawn rate troncos
+                    elementsChar = Utils.charLogs;
+                    colorsElements = Utils.colorsLogs.ToList();
+                }
+                else if(i > Utils.MAP_HEIGHT / 2)
+                {
+                    speedPlayer = false;
+                    background = ConsoleColor.Black;
+                    damageElements = true;
+                    damageBackground = false;
+                    elementsPercent = 0.3f; //Spawn rate coches
+                    elementsChar = Utils.charCars;
+                    colorsElements = Utils.colorsCars.ToList();
+                } else {
+                    speedPlayer = false;
+                    background = ConsoleColor.DarkGreen;
+                    damageElements = false;
+                    damageBackground = false;
+                    elementsPercent = 0f;
+                    elementsChar = ' ';
+                    colorsElements = Utils.colorsCars.ToList();
+                }
+                Lane lane = new Lane(i, speedPlayer, background, damageElements, damageBackground, elementsPercent, elementsChar, colorsElements);
+                lanes.Add(lane);
+            }
+
+            Lane lane0 = new Lane(0, false, ConsoleColor.DarkGreen, false, false, 0f, ' ', colorsElements);
+            Lane laneEnd = new Lane(Utils.MAP_HEIGHT-1, false, ConsoleColor.Black, false, false, 0f, ' ', colorsElements);
+            lanes.Add(lane0);
+            lanes.Add(laneEnd);
+
+            while (gameState == Utils.GAME_STATE.RUNNING)
+            {
+                Console.Clear();
+
+                foreach (Lane lane in lanes)
+                {
+                    lane.Update();
+                    lane.Draw();
+                }
+                player.Draw(lanes);
+
+                Vector2Int input = Utils.Input();
+
+                gameState = player.Update(input, lanes);
+
+                if (gameState == Utils.GAME_STATE.WIN)
+                {
+                    Console.WriteLine("You Win!");
+                    break;
+                }
+                else if (gameState == Utils.GAME_STATE.LOOSE)
+                {
+                    Console.WriteLine("You lose...");
+                    break;
+                }
+                TimeManager.NextFrame();
+            }
+            TimeManager.timer.Stop();
+        }
+    }
+}
---
+++ M05_UF3_P3_Frogger-AntonioManuelÁlvarezMadueño/M05_UF3_P3_Frogger/Utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M05_UF3_P3_Frogger
+{
+    public class Vector2Int // 2 Warnings¿?
+    {
+        public int x;
+        public int y;
+        public Vector2Int(int x = 0, int y = 0) { this.x = x; this.y = y; }
+
+        public static Vector2Int zero { get { return new Vector2Int(); } }
+        public static Vector2Int right { get { return new Vector2Int(x: 1); } }
+        public static Vector2Int left { get { return new Vector2Int(x: -1); } }
+        public static Vector2Int up { get { return new Vector2Int(y: -1); } }
+        public static Vector2Int down { get { return new Vector2Int(y: 1); } }
+
+        public override bool Equals(object obj) //Compara un objeto con las coordenadas del vector ¿?
+        {
+            if(obj is Vector2Int)
+            {
+                return this == (Vector2Int)obj;
+            }
+            return base.Equals(obj);
+        }
+
+        public override string ToString() //Pasa un vector a string.
+        {
+            return "(" + x + ", " + y + ")";
+        }
+
+        //Comparadores para vectores.
+        public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new Vector2Int(a.x + b.x, a.y + b.y);
+        public static bool operator ==(Vector2Int a, Vector2Int b) => a.x == b.x && a.y == b.y;
+        public static bool operator !=(Vector2Int a, Vector2Int b) => a.x != b.x || a.y != b.y;
+    }
+    public static class Utils
+    {
+        public static Random rnd = new Random();
+        public const int MAP_WIDTH = 20; //ancho
+        public static int MAP_HEIGHT = 13; //alto
+
+        public const char charCars = '╫'; //"Sprite" y colores de los coches.
+        public static readonly ConsoleColor[] colorsCars = { ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.Red };
+        public const char charLogs = '='; //"Sprite" y colores de los troncos.
+        public static readonly ConsoleColor[] colorsLogs = { ConsoleColor.DarkYellow, ConsoleColor.Yellow};
+
+        public enum GAME_STATE { RUNNING, WIN, LOOSE };
+
+        public static Vector2Int Input() //Modifica los vectores según los inputs de jugador.
+        {
+            if(Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow:
+                        return Vector2Int.left;
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        return Vector2Int.up;
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        return Vector2Int.right;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        return Vector2Int.down;
+                }
+            }
+            return Vector2Int.zero;
+        }
+    }
+} 
